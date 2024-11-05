7e5fc22e0c17c3ea8554670be34ad95764c628a5
Maciej Trybilo <maciektr@gmail.com>
2024-11-05 18:46:59 +0100

    fix

--- Package.swift
+++ Package.swift
@@ -5,8 +5,11 @@ import PackageDescription
 
 let package = Package(
     name: "htmltagclass-generator",
+    products: [
+        .executable(name: "htmltagclass-generator", targets: ["htmltagclass-generator"])
+    ],
     dependencies: [
-      .package(url: "https://github.com/apple/swift-argument-parser", from: "1.0.0"),
+        .package(url: "https://github.com/apple/swift-argument-parser", from: "1.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
